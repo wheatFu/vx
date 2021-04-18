e1cfeedb3f7fce9de174e389f8321deed5764dc4
Wheat fu <gfu@knx.com.cn>
2021-04-18 22:18:16 +0800

    fix

--- src/app/modules/auth/login/index.component.ts
+++ src/app/modules/auth/login/index.component.ts
@@ -123,7 +123,7 @@ export class LoginComponent implements OnDestroy {
         this.tokenService.set(res.user)
         // 重新获取 StartupService 内容，我们始终认为应用信息一般都会受当前用户授权范围而影响
         this.startupSrv.load().then(() => {
-          this.menuService.add(res?.menu)
+          this.menuService.add(res.menu)
           let url = this.tokenService.referrer!.url || '/'
           if (url.includes('/passport')) {
             url = '/'
